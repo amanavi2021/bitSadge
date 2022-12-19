3de8fa70d69c8bca13802114c1a9ea66c4d63666
amanavi2021 <105625270+amanavi2021@users.noreply.github.com>
2022-12-19 09:22:55 +0000

    Deploying to gh-pages from @ amanavi2021/bitSadge@c2486a548a705dcfbc28cd1363a4fac3a67c5915 🚀

---
+++ index.00f1e251.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({dots:!0,arrows:!1}),$(".animation").slick({arrows:!1,autoplay:!0,autoplaySpeed:5e3})}));
+//# sourceMappingURL=index.00f1e251.js.map
---
+++ index.c2b9dcaf.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({dots:!0,arrows:!1}),$(".animation").slick({arrows:!1,autoplay:!0,autoplaySpeed:5e3})}));
+//# sourceMappingURL=index.c2b9dcaf.js.map
