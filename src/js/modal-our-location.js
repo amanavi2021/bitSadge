5e1085c952af67dfea6fa7af1886c7da73d3fc1c
Natalia <nataliamaliovana.nm@gmail.com>
2022-12-19 13:46:22 +0700

    finished modal Our Location

--- src/js/modal-our-location.js
+++ src/js/modal-our-location.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-location-open]"),
+    closeModalBtn: document.querySelector("[data-modal-location-close]"),
+    modal: document.querySelector("[data-modal-location]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
