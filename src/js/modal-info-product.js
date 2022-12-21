d3fed0a9f7d433c94dc70239938d602348ee1c2c
Vlad <vicradrt@gmail.com>
2022-12-21 21:44:48 +0200

    Fised
    pre-alpha 0.5

--- src/js/modal-info-product.js
+++ src/js/modal-info-product.js
@@ -1,8 +1,25 @@
+// Modal green
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal__open--ingredients-green]"),
       closeModalBtn: document.querySelector("[data-modal__close--ingredients-green]"),
       modal: document.querySelector("[data-modal__ingredients-green]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// Modal yellow
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal__open--ingredients-yellow]"),
+      closeModalBtn: document.querySelector("[data-modal__close--ingredients-yellow]"),
+      modal: document.querySelector("[data-modal__ingredients-yellow]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
