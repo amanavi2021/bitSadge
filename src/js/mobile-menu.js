6b1b597de18d444ca80b123de36d6ff04f6179d0
Vlad <vicradrt@gmail.com>
2022-12-23 12:52:27 +0200

    Fixed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,19 +2,20 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const closeMenuLink = document.querySelectorAll('.modal-menu__link');
+  const buyNowBtn = document.querySelector('.buy-now-button--tablet-none');
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
-
+closeMenuLink.forEach(item => item.addEventListener("click", toggleMenu));
+buyNowBtn.addEventListener("click", toggleMenu);
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
